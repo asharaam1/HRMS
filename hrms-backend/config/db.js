b097c5b8bd43259e3ba5735257d6a14a695b203f
asharaam1 <lohanaasharaam1@gmail.com>
2025-10-01 23:06:08 +0500

    model and controller

--- hrms-backend/config/db.js
+++ hrms-backend/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
